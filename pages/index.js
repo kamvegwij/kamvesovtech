04e3b4f8a941ca21c018c4085e00c8b12a6965f1
Kamve Gwijana <gwijgwij011@gmail.com>
2022-11-13 10:42:39 +0200

    add scroll bar, animations

--- pages/index.js
+++ pages/index.js
@@ -2,38 +2,85 @@ import Head from 'next/head'
 import { createElement } from 'react'
 
 export default function Home() {
-
+  var id = null;
+  
+  function animateItem(x){
+    var edge = 0; //used to break
+    //change style properties of element x
+    clearInterval(id);
+    id  = setInterval(frame, 80);
+    //now define frame function and animate list of items:
+    
+    
+    function frame(){
+      for (let i = 0; i < x.length; i++){
+        if (edge == 10){
+          clearInterval(id);
+          x[i].style.border = 'none'; //I want to now clear the border
+        }
+        else{
+          edge += .5;
+          x[i].style.border = edge + 'px solid #e2d28c';
+          x[i].style.borderTop = 'none';
+        }
+    }
+  }//end of loop
+  }
   const itemClick = (e) => {
     console.log(e.target);//test which item selected
+
+    //I also want to highlight the tech used for the different projects
+
+    //i will later then change some of its style properties when clicked.
+    //get first element of nodelist returned by getElementsByName() using index 0
+    const python = document.getElementsByName("python")[0];
+    const css = document.getElementsByName("css")[0];
+    const godot = document.getElementsByName("godot")[0];
+    const java = document.getElementsByName("java")[0];
+    const javascript = document.getElementsByName("javascript")[0];
+    const captivate = document.getElementsByName("captivate")[0];
+    const html = document.getElementsByName("html")[0];
+
     //now for conditions to allow me to show the description for each project:
     const para = document.getElementById("descriptions");
-
     if (e.target.id == "heybud"){
       para.innerHTML = `This is a short top-down adventure game I made using Godot and wrote in GDScript. 
                         You begin in the characters farm, 
                         his name Buddy, and are instructed to find his tools that have gone missing. Once every item is collected you are prompted
                         to go to his house to then enter the next level. There is currently only one level.`;
+      animateItem([godot]); //I call the function with a parameter of the list of tech used.
+      
     }
     else if (e.target.id == "word"){
       para.innerHTML = `This is a simple single page web app with a main purpose of randomly displaying inspirational quotes to get your day going.
                         I made use of the Type.fit API to fetch the quotes and I did some string manipulation in JavaScript to display
                         this data in an aesthetic way.`;
-    }
+      animateItem([html, css, javascript, python]);
+                      }
     else if (e.target.id == "cinema"){
       para.innerHTML = `This is one of the two final year projects in Computer Science and it is web app that allows a user to
                         choose a featured movie, book a seat and also allow them to cancel the seat. This user data is then stored
                         in a database for admin purposes. I worked mainly on backend in this project using Flask.`;
-    }
+      animateItem([html, css, javascript, python]);
+                      }
     else if (e.target.id == "help"){
       para.innerHTML = `This is the last final year project and it is a web app that allows a user to login and have access to a page with helplines
                         in which they can be immediately taken to a call with one or they can access the chatroom where they can have a one on one
                         talk with a psychologist registered on our service. In this project I was a fullstack developer as I made the pages,
                         flask implementation, API data fetching and some of the main interactions.`;
-    }
+      animateItem([html, css, javascript, python]);
+                      }
     else if (e.target.id == "game"){
       para.innerHTML = `This is a project I am working on with a team for a company and I am a backend developer. We're using Godot and GDScript to
                         make an educational video game.`;
-    }
+      animateItem([godot]);    
+                      }
+
+  }
+
+  const showStack = (item) => {
+    console.log(item);//tech used held here.
+    items = ["python, css, godot, html, java, javascript, captivate"]//this is an array of the tech used
   }
 
   return (
@@ -65,30 +112,30 @@ export default function Home() {
                 <h1>My Projects</h1>
                 </div>
 
-                <h2>Hey Buddy - Top Down Adventure Game</h2>
+                <h2>1. Hey Buddy - Top Down Adventure Game</h2>
                 <img src="heybud.jpg" height="300" width="550" id="heybud" onClick={itemClick}></img>
                 <br></br><label id="add_info">Built with Godot and GDScript.</label>
                 <br></br><a href="https://github.com/kamvegwij/HeyBuddyProject">Source Code</a>
                 <br></br><a href="https://youtu.be/DEqJ1oXKJUI">Gameplay Runthrough</a>
 
-                <h2>What's The Word Today?</h2>
+                <h2>2. What's The Word Today?</h2>
                 <img src="word.jpg" height="300" width="550" id="word" onClick={itemClick}></img>
                 <br></br><label id="add_info">Built with HTML, CSS, JavaScript, Flask, Type.fit API and deployed on Heroku.</label>
                 <br></br><a href="https://github.com/kamvegwij/WordOfTheDay">Source Code</a>
                 <br></br><a href="https://whatsthewordtoday.herokuapp.com/">Deployed Version</a>
 
-                <h2>Cinema Booking</h2>
+                <h2>3. Cinema Booking</h2>
                 <img src="akatsukicinema.jpg" height="300" width="550" id="cinema" onClick={itemClick}></img>
                 <br></br><label id="add_info">Built with HTML, CSS, JavaScript, Flask and SQL.</label>
                 <br></br><a href="https://github.com/kamvegwij/HeyBuddyProject">Source Code </a>
 
-                <h2>QuickHelp</h2>
+                <h2>4. QuickHelp</h2>
                 <img src="quickhelp.jpg" height="300" width="550" id="help" onClick={itemClick}></img>
                 <br></br><label id="add_info">Built with HTML, CSS, JavaScript, Flask, SQL, Type.fit API, Twilio.</label>
                 <br></br><a href="https://github.com/kamvegwij/FINAL-CAPSTONE">Source Code</a>
                 <br></br><a href="https://myquickhelp.herokuapp.com/">Deployed Version </a>
 
-                <h2>Educational Video Game</h2>
+                <h2>5. Educational Video Game</h2>
                 <img src="bg3.png" height="300" width="550" id="game" onClick={itemClick}></img>
                 <br></br><label id="add_info">Built with Godot and GDScript.</label>
 
@@ -104,20 +151,21 @@ export default function Home() {
           <h1>Technologies I've Used</h1>
           <div id="tech">
             
-            <img src="pythonbg.png" id="tech_img"></img>
-            <img src="cssbg.png" id="tech_img"></img>
-            <img src="godot.png" id="tech_img"></img>
-            <img src="htmlbg.png" id="tech_img"></img>
-            <img src="javabg.png" id="tech_img"></img>
-            <img src="jsbg.png" id="tech_img"></img>
-            <img src="captivatebg.png" id="tech_img"></img>
+            <img src="pythonbg.png" id="tech_img" name="python"></img>
+            <img src="cssbg.png" id="tech_img" name="css"></img>
+            <img src="godot.png" id="tech_img" name="godot"></img>
+            <img src="htmlbg.png" id="tech_img" name="html"></img>
+            <img src="javabg.png" id="tech_img" name="java"></img>
+            <img src="jsbg.png" id="tech_img" name="javascript"></img>
+            <img src="captivatebg.png" id="tech_img" name="captivate"></img>
 
           </div>
           <div id="bottom-container">
           <div id="bottom-info">
               <h1>Why do I want to join SovTech?</h1>
               <p>I'm a young and enthusiastic developer and I'm ready to challenge myself by aquiring more skills and knowledge in the
-                 field of Software Development. Watching the video on the SovTech site, I would honoured to be in a work environment 
+                 field of Software Development. Watching the video on the SovTech site, I would be honoured to be in a work environment like this
+                 and ultimately be around people with an equal or greater passion about tech.
               </p>
           </div>
           </div>
@@ -132,13 +180,15 @@ export default function Home() {
         {`
         /*headers styling*/
           #top-container{
+            padding: 10px;
             width: 100%;
             background: linear-gradient(96deg, #2d270b 50%, #544815 50.1%);
             color: white;
             border-radius: 10px;
           }
           #main-head{
-            font-size:2em;
+            font-size:2.5em;
+            letter-spacing: 3px;
             margin-left: 1em;
             text-decoration: underline wavy #d3bb52;
             text-underline-position: under;
@@ -147,7 +197,8 @@ export default function Home() {
             display: inline;
           }
           h1{
-            font-size: 35px;
+            font-size: 2em;
+            letter-spacing: 3px;
             -webkit-text-fill-color: transparent;
             -webkit-text-stroke: 1px;
           }
@@ -172,6 +223,7 @@ export default function Home() {
         /*projects section styling*/
 
         #work-container{
+          padding: 20px;
           justify-content: center;
           display: flex;
         }
@@ -185,26 +237,39 @@ export default function Home() {
           height: 50vh;
           margin: 4px;
           overflow: scroll;
+          overflow-x: hidden;
         }
         #projects::-webkit-scrollbar{
-          width: 12px;
+          width: 5px;
+          
         }
-        #projects::-webkit-scrollbar{ /*hide scrollbars*/
-          display: none;
+        #projects::-webkit-scrollbar-track{ /*hide scrollbars*/
+          background: transparent;
+        }
+        #projects::-webkit-scrollbar-thumb{
+          border-radius: 20px;
+          background-color: white;
+        }
+        #projects::-webkit-scrollbar-thumb:hover{
+          background-color: rgba(255, 255, 255, .6);
         }
         #projects:hover{
           cursor: grab;
+          
         }
         #projects img {
           width: 80%;
           height: 70%;
         }
+       /* background: linear-gradient(45deg,#e2d28c, #544815);*/
         #proj-head{
           width: 100%;
           position: sticky;
           top: 0;
-          border: none;
-          background: linear-gradient(45deg,#544815, #e2d28c);
+        }
+        #proj-head h1, #descr-head h1{
+          display: inline;
+          border-bottom: 4px solid #e2d28c;
         }
         #add_info{
           color: rgba(1, 0, 0, .4);
@@ -214,7 +279,6 @@ export default function Home() {
           position: sticky;
           top: 0;
           border: none;
-          background: linear-gradient(45deg,#e2d28c, #544815);
         }
         #description{
           border-radius: 5px;
@@ -235,8 +299,7 @@ export default function Home() {
         #tech #tech_img{
           margin:.5em;
           width: 5%;
-          height: 40%;
-          border-radius: 50%;
+          height: 50%;
         }
 
         /*bottom section styling*/
@@ -267,9 +330,11 @@ export default function Home() {
       <style jsx global>
         {`
         @import url('https://fonts.googleapis.com/css2?family=Changa:wght@200');
+        @import url('https://fonts.googleapis.com/css2?family=Dosis:wght@200');
         main{
+              padding: 2px;
               text-align: center;
-              font-family: 'Changa', sans-serif;
+              font-family: 'Dosis', sans-serif;
               color: black;
               font-weight: bold;
               font-size: 100%;
